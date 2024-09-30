881ff806463f98a677de774b2972262e431e692f
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-09-30 11:51:44 +0200

    Cars

---
+++ resources/views/cars.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot name='title'>Cars</x-slot>
+
+    <p>This is car page.</p>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\carController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+
+Route::get('/cars', [carController::class, 'showCars']);
