1f1b5720837e16d50de9a9ff283c949378577cfd
Horvath Mark <horvath.mark1@students.jedlik.eu>
2024-09-30 12:24:46 +0200

    kartyak

--- app/Http/Controllers/carController.php
+++ app/Http/Controllers/carController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class carController extends Controller
 {
+    private $cars;
+
+    function __construct()
+    {
+        // $this->cars = [
+        //     'Ford',
+        //     'BMW',
+        //     'Mercedes'
+        // ];
+
+        $this->cars =  Storage::json('cars2.json')["cars"];
+    }
+
     function showCars(){
 
-        return view('cars');
+        return view('cars', ['cars' => $this->cars]);
     }
 }
--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -1,5 +1,19 @@
 <x-layout>
     <x-slot name='title'>Cars</x-slot>
 
-    <p>This is car page.</p>
+    <h2>Available cars</h2>
+    <div class="row">
+        @foreach ($cars as $car )
+        <div class="col-md-3">
+        <div class="card" style="width: 18rem;">
+                <img src="{{ $car['image'] }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                <h5 class="card-title">{{ $car['title'] }}</h5>
+                <p class="card-text">Start prdocution: {{ $car['start_production'] ?? '---'}}</p>
+                <p class="card-text">Class: {{ $car['class'] ?? '---'}}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </x-layout>
